6a56ffad59723c1abdce12e2b8de049fcb559d01
BluePigman <mpan38@gmail.com>
2021-07-04 20:16:07 -0400

    Rename config

--- config.py
+++
@@ -1,6 +0,0 @@
-OAUTH_TOKEN = 'YOUR OAUTH TOKEN'
-username = 'BOT NAME'
-channels = ['Channel List']
-prefix = '<' #Desired Prefix.
-bot_owner = "Bot Owner" # For private commands.
-
---
+++ config_example.py
@@ -0,0 +1,6 @@
+OAUTH_TOKEN = 'YOUR OAUTH TOKEN'
+username = 'BOT NAME'
+channels = ['Channel List']
+prefix = '<' #Desired Prefix.
+bot_owner = "Bot Owner" # For private commands.
+
