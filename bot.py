1de8741ca61c4d1313352b08260bd470b7a7c043
BluePigman <mpan38@gmail.com>
2021-07-24 12:14:13 -0400

    Add pyramid commands

--- bot.py
+++ bot.py
@@ -76,7 +76,9 @@ class Bot:
             'ro': self.reply_with_random_opening,
             'joke': self.reply_with_joke,
             'r960': self.reply_with_random960,
-            'help_ro': self.reply_with_help_ro
+            'help_ro': self.reply_with_help_ro,
+            'pyramid': self.reply_with_pyramid,
+            'slow_pyramid': self.reply_with_slow_pyramid
         }
         
         #only bot owner can use these commands
@@ -345,8 +347,59 @@ class Bot:
             self.joke += 1
             if self.joke > 84:
                 self.joke = 0
-        
-   
+
+    #https://stackoverflow.com/q/6266727            
+    def reply_with_pyramid(self,message):
+        if (message.user not in self.state or time.time() - self.state[message.user] > 
+            self.cooldown):
+            self.state[message.user] = time.time()
+            if len(message.text_args) > 1 and message.text_args[1].isnumeric():
+                emote = message.text_args[0]
+                width = int(message.text_args[1])
+                if len(emote) * length + length - 1 < 500:
+                    text = ''
+                    for x in range(length): # go up
+                        text += (emote + ' ')
+                        self.send_privmsg(message.channel, text)
+                        time.sleep(0.1)
+                    for y in range(length): #go down
+                        text = text.rsplit(emote, 1)[0]
+                        self.send_privmsg(message.channel, text)
+                        time.sleep(0.1)
+                else:
+                    text = 'Pyramid is too large to be displayed in chat. Use \
+                    a smaller pyramid width.'
+                    self.send_privmsg(message.channel, text)
+
+            else:
+                text = 'Width must be an integer. Usage: #pyramid {name} {width}'
+                self.send_privmsg(message.channel, text) 
+
+    def reply_with_slow_pyramid(self,message):
+        if (message.user not in self.state or time.time() - self.state[message.user] > 
+            self.cooldown):
+            self.state[message.user] = time.time()
+            if len(message.text_args) > 1 and message.text_args[1].isnumeric():
+                emote = message.text_args[0]
+                length = int(message.text_args[1])
+                if len(emote) * length + length - 1 < 500:
+                    text = ''
+                    for x in range(length): # go up
+                        text += (emote + ' ')
+                        self.send_privmsg(message.channel, text)
+                        time.sleep(1.6)
+                    for y in range(length): #go down
+                        text = text.rsplit(emote, 1)[0]
+                        self.send_privmsg(message.channel, text)
+                        time.sleep(1.6)
+                else:
+                    text = 'Pyramid is too large to be displayed in chat. Use \
+                    a smaller pyramid width.'
+                    self.send_privmsg(message.channel, text)
+
+            else:
+                text = 'Width must be an integer. Usage: #pyramid {name} {width}'
+                self.send_privmsg(message.channel, text)
                 
     """ Chess commands """
 
@@ -375,7 +428,6 @@ for a specific side, and/or add a name for search. e.g. #ro King's Indian \
             self.cooldown):
             self.state[message.user] = time.time()
             self.send_privmsg(message.channel, text)
-
 
     """Private commands"""
     
