51ae3670aec1720693111ab3c3212e0201605430
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-05-11 00:02:39 -0400

    Add required tokens for timeout

--- config_example.py
+++ config_example.py
@@ -4,3 +4,5 @@ channels = ['Channel List']
 prefix = '<' #Desired Prefix.
 bot_owner = "Bot Owner" # For private commands.
 db_uri = "Your MongoDB URI"
+user_access_token = "Your user access token"
+client_id = "Your client ID"
