bf68c68d514cd9a4c52d054ffd9a2b7bfb2c5d9b
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-05-26 23:11:00 -0400

    add help for ascii command

---
+++ Commands/help_ascii.py
@@ -0,0 +1,23 @@
+import time
+
+def reply_with_help_ascii(self, message):
+    help_msg = f"usage: {self.command_prefix}ascii " + "[Emote] [-w] [-h] [-r {90,180,270}] [-tr] [-d] [-b] [-e] [-i] [-g] [-t]"
+    help_msg2 = """Generate ascii art from emote link.
+    Optional arguments:
+    -w {int} width of ascii
+    -h {int} height of ascii
+    -r {90,180,270} rotate ascii clockwise
+    -tr {0-255} level to apply threshold dithering
+    -d Use Floyd-Steinberg dithering.
+    -b Remove transparent background.
+    -e Keep empty characters.
+    -i Invert the end result.
+    -g Use multiple frames of the first gif provided.
+    -t {text} Text to print below the ASCII. (TODO)""".replace("\n", " ")
+
+    if (message.user not in self.state or time.time() - self.state[message.user] >
+            self.cooldown):
+        self.state[message.user] = time.time()
+        self.send_privmsg(message.channel, help_msg)
+        time.sleep(0.69)
+        self.send_privmsg(message.channel, help_msg2)
--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import socket, sys, ssl, time, config, chess
 from collections import namedtuple
 from pymongo.mongo_client import MongoClient
-from Commands import ( bot_info, date, ping, help_chess, source_code, play_chess, ro, r960, help_ro, pyramid, slow_pyramid,
+from Commands import ( bot_info, date, help_ascii, ping, help_chess, source_code, play_chess, ro, r960, help_ro, pyramid, slow_pyramid,
 news, help_news, daily, roulette, balance, leaderboard, help, shop, timeout, trophies, gemini, gemini2, llama, llama3,
-ascii )
+ascii, help_ascii )
 
 
 Message = namedtuple(
@@ -68,7 +68,8 @@ class Bot:
             'gemini2': gemini2.reply_with_gemini_experimental,
             'llama': llama.reply_with_llama,
             'llama3': llama3.reply_with_llama3,
-            'ascii': ascii.reply_with_ascii
+            'ascii': ascii.reply_with_ascii,
+            'help_ascii': help_ascii.reply_with_help_ascii
         }
 
         # only bot owner can use these commands
