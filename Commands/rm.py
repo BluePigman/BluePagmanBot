9889d19ee04dfea65d672549b22a78f166caaac0
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-07-12 16:08:21 -0400

    use different method of getting random reddit post
    scrape from a redlib frontend instance, https://l.opnxng.com/info

--- Commands/rm.py
+++ Commands/rm.py
@@ -1,6 +1,8 @@
+import json
 import random
 import time
 import requests
+from bs4 import BeautifulSoup
 def reply_with_rm(self, message):
     if (message['source']['nick'] not in self.state or time.time() - self.state[message['source']['nick']] > self.cooldown):
         self.state[message['source']['nick']] = time.time()
@@ -9,30 +11,85 @@ def reply_with_rm(self, message):
             subreddit = "all"
         else:
             subreddit = message['command']['botCommandParams'].split()[0]
-        url = f"https://reddit.com/r/{subreddit}/hot.json"
-
-        response = requests.get(url)
-        if response.status_code == 403:
-            self.send_privmsg(message['command']['channel'], "Reddit is rate limited! Wait 10 minutes for more.")
-            return
-        data = response.json()
-        posts = data["data"]["children"]
-        if len(posts) == 0:
-            self.send_privmsg(message['command']['channel'], "Subreddit not found!")
-            return
-        posts = [post for post in posts if not post['data']['stickied']]
         
-        post = random.choice(posts)
-        url =  post["data"]["url"]
-        subName = post["data"]["subreddit_name_prefixed"]
-        title = post["data"]["title"]
-        score = post["data"]["score"]
-        created_utc = post["data"]["created_utc"]
-        createdAgo = format_time_ago(created_utc)
-        msg = f"{subName}: {title} {url} (Score: {score}, posted {createdAgo})"
+        post_dict = scrape_subreddit(subreddit)
+        if not post_dict:
+            self.send_privmsg(message['command']['channel'], "No posts found.")
+            return
+        subName = post_dict['subreddit']
+        title = post_dict['title']
+        url =  post_dict['link']
+        score = post_dict['score']
+        time_posted = post_dict['time_posted']
+        msg = f"{subName}: {title} {url} (Score: {score}, posted {time_posted})"
 
         self.send_privmsg(message['command']['channel'], msg)
 
+
+
+def scrape_subreddit(subreddit):
+    url = f"https://l.opnxng.com/r/{subreddit}"
+    response = requests.get(url)
+    html_content = response.content
+
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, features="html.parser",from_encoding='utf-8')
+
+    # Find all posts
+    posts = soup.find_all("div", class_="post")
+
+    post_data = []
+    for post in posts:
+        if "post stickied" == " ".join(post.get("class", [])):
+            continue
+        title_links = post.find("h2", class_="post_title").find_all("a")
+        if len(title_links) == 1:
+            title = title_links[0].get_text(strip=True)
+        elif len(title_links) > 1:
+            title = title_links[1].get_text(strip=True)
+        else:
+            title = None
+        title = title.replace("\u2019", "'") if title else None
+        title = title.replace('\u201c', '"') if title else None
+        title = title.replace('\u201d', '"') if title else None
+
+        media_link = post.find("div", class_="post_media_content")
+        thumbnail_link = post.find("a", class_="post_thumbnail")
+        
+        if media_link:
+            link = media_link.find("a")["href"]
+            link = f"https://l.opnxng.com{link}"
+        elif thumbnail_link:
+            link = thumbnail_link["href"]
+            link = link
+        elif len(title_links) > 0:
+            link = title_links[0]["href"]
+            link = link
+        else:
+            link = None
+
+        if link and link.startswith("/r/"):
+            link = f"https://l.opnxng.com{link}"
+
+        # Extract the time posted and score
+        time_posted = post.find("span", class_="created")["title"] if post.find("span", class_="created") else None
+        score = post.find("div", class_="post_score").get_text(strip=True).replace(' Upvotes', '') if post.find("div", class_="post_score") else None
+        score = score.replace('Upvotes', '') if score else None
+        score = score.replace("\u2022", "Idk") if score else None
+        
+        subreddit = post.find("a", class_="post_subreddit").get_text(strip=True)
+        # Create a dictionary for the post
+        post_dict = {
+            "title": title,
+            "link": link,
+            "time_posted": time_posted,
+            "score": score,
+            "subreddit": subreddit
+        }
+        post_data.append(post_dict)
+    if not post_data:
+        return None
+    return(random.choice(post_data))
 
 def format_time_ago(created_utc):
     now = time.time()
