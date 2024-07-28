3cdb75235acf1291cef19068bb02d9a555ceea33
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-07-28 12:30:19 -0400

    Remove Llama

--- Commands/llama.py
+++ Commands/llama.py
@@ -1,5 +1,4 @@
 import time
-import ollama
 
 """
 Instructions:
@@ -16,30 +15,4 @@ def reply_with_llama(self, message):
             self.cooldown):
         self.state[message['source']['nick']] = time.time()
 
-    if not message['command']['botCommandParams']:
-        m = f"@{message['tags']['display-name']}, please provide a prompt. Model: llama2-uncensored, \
-            https://ollama.com/library/llama2-uncensored"
-        self.send_privmsg(message['command']['channel'], m)
-        return
-
-    prompt = message['command']['botCommandParams']
-    self.send_privmsg(message['command']['channel'], "Result usually takes over a minute. Please wait.")
-    result = generate(prompt)
-    self.send_privmsg(message['command']['channel'], f"@{message['tags']['display-name']},")
-    time.sleep(1) 
-    for m in result:
-        self.send_privmsg(message['command']['channel'], m)
-        time.sleep(1)
-
-
-def generate(prompt):
-    try:
-        result = ollama.generate(model='llama2-uncensored', prompt=prompt, stream=False,
-                                options= {"num_ctx": 1024})["response"]
-        n = 495
-        result = result.replace('\n', ' ')
-        return [result[i:i+n] for i in range(0, len(result), n)]
-
-    except Exception as e:
-        print(e)
-        return ["Some error occurred."]
+        self.send_privmsg(message['command']['channel'], "The Llama command has been removed.")
--- Commands/llama3.py
+++ Commands/llama3.py
@@ -1,5 +1,4 @@
 import time
-import ollama
 
 """
 Instructions:
@@ -16,31 +15,4 @@ def reply_with_llama3(self, message):
             self.cooldown):
         self.state[message['source']['nick']] = time.time()
 
-    if not message['command']['botCommandParams']:
-        m = f"@{message['tags']['display-name']}, please provide a prompt. Model: Dolphin 2.9 Llama 3, \
-            https://ollama.com/library/dolphin-llama3"
-        self.send_privmsg(message['command']['channel'], m)
-        return
-
-    prompt = message['command']['botCommandParams']
-    self.send_privmsg(message['command']['channel'], "Result usually takes a few minutes. Please wait.")
-    result = generate(prompt)
-    self.send_privmsg(message['command']['channel'], f"@{message['tags']['display-name']},")
-    time.sleep(1) 
-    for m in result:
-        self.send_privmsg(message['command']['channel'], m)
-        time.sleep(1)
-
-
-def generate(prompt):
-    try:
-        result = ollama.generate(model='dolphin-llama3', prompt=prompt, stream=False,
-                                options= {"num_ctx": 1024})["response"]
-        n = 495
-        result = result.replace('\n', ' ')
-        result = result.replace('\r\n', ' ')
-        return [result[i:i+n] for i in range(0, len(result), n)]
-
-    except Exception as e:
-        print(e)
-        return ["Some error occurred."]
+        self.send_privmsg(message['command']['channel'], "The Llama command has been removed.")
