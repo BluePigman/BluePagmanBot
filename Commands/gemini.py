a63b139bcf99e7dcbdfae4bdf3bf2e2f4bab863c
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-10-18 18:32:38 -0400

    add specific gemini prompt for emote descriptions

--- Commands/gemini.py
+++ Commands/gemini.py
@@ -2,6 +2,8 @@ import time
 import vertexai
 from vertexai.generative_models import GenerativeModel, SafetySetting
 
+vertexai.init(project="bluepagmanbot", location="us-central1")
+
 
 def reply_with_gemini(self, message):
     if (message['source']['nick'] not in self.state or time.time() - self.state[message['source']['nick']] >
@@ -48,18 +50,16 @@ safety_settings = [
 
 
 def generate(prompt) -> list[str]:
-    vertexai.init(project="bluepagmanbot", location="us-central1")
-    model = GenerativeModel(
-        "gemini-1.5-flash-002",
-        system_instruction=["""Please always provide a short and concise response. Do not ask the user follow up questions, 
-                        because you are intended to provide a single response with no history and are not expected
-                        any follow up prompts. If given a media file, please describe it. For GIFS/WEBP files describe all frames.
-                        Answer should be at most 990 characters."""]
-    )
     try:
         if isinstance(prompt, str):
             prompt = [prompt]
-
+        model = GenerativeModel(
+            "gemini-1.5-flash-002",
+            system_instruction=["""Please always provide a short and concise response. Do not ask the user follow up questions, 
+                        because you are intended to provide a single response with no history and are not expected
+                        any follow up prompts. If given a media file, please describe it. For GIFS/WEBP files describe all frames.
+                        Answer should be at most 990 characters."""]
+        )
         response = model.generate_content(
             prompt,
             generation_config=generation_config,
@@ -69,6 +69,28 @@ def generate(prompt) -> list[str]:
         response = response.replace('*', ' ')
         n = 495
         return [response[i:i+n] for i in range(0, len(response), n)]
+    except Exception as e:
+        print(e)
+        return ["Error: ", e[0:490]]
+
+
+def generate_emote_description(prompt):
+    system_instruction = [
+        "You don't need to say Here's a description, just say the result."]
+    try:
+        model = GenerativeModel(
+            "gemini-1.5-flash-002",
+            system_instruction=system_instruction
+        )
+        response = model.generate_content(
+            prompt,
+            generation_config=generation_config,
+            safety_settings=safety_settings,
+            stream=False,
+        ).text.replace('\n', ' ')
+        response = response.replace('*', ' ')
+        n = 495
+        return response
     except Exception as e:
         print(e)
         return ["Error: ", e[0:490]]
