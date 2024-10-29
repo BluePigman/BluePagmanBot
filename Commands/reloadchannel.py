52a6aa51d2ca4a37c72841c2e8a269a8f8dd9104
BluePigman <82780692+BluePigman@users.noreply.github.com>
2024-10-28 21:10:16 -0400

    Remove outdated channel emotes from ChannelEmotes
    Emotes that are in the database that aren't in the channel anymore are now removed from ChannelEmotes table.

--- Commands/reloadchannel.py
+++ Commands/reloadchannel.py
@@ -11,13 +11,17 @@ def reload_channel(self, message):
         channel_id = message["tags"]["room-id"]
         start_time = time.time()
 
-        # Combined current emote set for all platforms
-        current_emote_ids = set()
 
         emotes_collection = self.db['Emotes']
         channel_emotes_collection = self.db['ChannelEmotes']
         bulk_emote_operations = []
         bulk_channel_operations = []
+
+        existing_emotes = channel_emotes_collection.find({"channel_id": channel_id})
+        existing_emote_ids = {emote["emote_id"] for emote in existing_emotes} # all emote ids before the reload.
+
+        # Combined current emote set for all platforms, will be filled in with emotes fetched from reload.
+        current_emote_ids = set()
 
         # Helper function to process emotes from different platforms
         def process_emotes(emotes, emote_type, id_key="id", name_key="name", url_template=""):
@@ -36,7 +40,7 @@ def reload_channel(self, message):
                 )
                 current_emote_ids.add(emote_id)
 
-                # Upsert channel-emote relationship
+                # Upsert channel-emote relationship (if no document found then add it)
                 bulk_channel_operations.append(
                     pymongo.UpdateOne(
                         {"channel_id": channel_id, "emote_id": emote_id},
@@ -78,11 +82,10 @@ def reload_channel(self, message):
         if bulk_channel_operations:
             channel_emotes_collection.bulk_write(bulk_channel_operations)
 
-        # Delete emotes not in the current set for this channel
-        channel_emotes_collection.delete_many(
-            {"channel_id": channel_id, "emote_id": {
-                "$nin": list(current_emote_ids)}}
-        )
+        # Perform deletions for outdated emotes
+        to_delete = existing_emote_ids - current_emote_ids
+        if to_delete:
+            channel_emotes_collection.delete_many({"channel_id": channel_id, "emote_id": {"$in": list(to_delete)}})
 
         end_time = time.time()
         elapsed_time = end_time - start_time
