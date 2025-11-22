24262731f561c4786a77dd7488a47994ef2f8fa2
BluePigman <82780692+BluePigman@users.noreply.github.com>
2025-11-22 17:23:54 -0500

    Update testRandomRedditComment.py

--- Tests/testRandomRedditComment.py
+++ Tests/testRandomRedditComment.py
@@ -2,7 +2,8 @@ from Commands import rrc
 
 def main():
     for _ in range(5):
-        result = rrc.get_random_comment('nba')
+        posts_result = rrc.get_posts('nba')
+        result = rrc.get_random_comment(posts_result["posts_data"], 'nba')
         if result:
             print(result['comment_body'], result['comment_link'], f'\n')
         else:
@@ -11,4 +12,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
