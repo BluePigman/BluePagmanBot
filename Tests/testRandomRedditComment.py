82ad2fd7422c07fdce03aa1ca6ef62cbb663b009
BluePigman <82780692+BluePigman@users.noreply.github.com>
2025-11-30 09:47:12 -0500

    Make test for rrc more efficient

--- Tests/testRandomRedditComment.py
+++ Tests/testRandomRedditComment.py
@@ -1,8 +1,8 @@
 from Commands import rrc
 
 def main():
+    posts_result = rrc.get_posts('nba')
     for _ in range(5):
-        posts_result = rrc.get_posts('nba')
         result = rrc.get_random_comment(posts_result["posts_data"], 'nba')
         if result:
             print(result['comment_body'], result['comment_link'], f'\n')
